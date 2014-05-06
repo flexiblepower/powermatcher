fb93bf6f1e35c8d884e58045c50259acfeb93d5e
Jan Sipke van der Veen <jan_sipke.vanderveen@tno.nl>
2014-05-06 13:02:50 +0200

    Fixed listing of generated directories

--- net.powermatcher.simulation.gui/src/net/powermatcher/simulation/buildutil/GenerateTargetPlatformXml.java
+++ net.powermatcher.simulation.gui/src/net/powermatcher/simulation/buildutil/GenerateTargetPlatformXml.java
@@ -2,20 +2,17 @@ package net.powermatcher.simulation.buildutil;
 
 import java.io.File;
 
-
 public class GenerateTargetPlatformXml {
 
 	public static void main(String[] args) {
-		
-		File workspace = new File(System.getProperty("user.dir")).getParentFile();
-		
-		for(File dir : workspace.listFiles()) {
+		File workspace = new File("").getAbsoluteFile().getParentFile();
+
+		System.out.println("Listing files in " + workspace);
+		for (File dir : workspace.listFiles()) {
 			File generated = new File(dir.getAbsolutePath() + File.separator + "generated");
-			if(dir.isDirectory() && generated.exists()) {
+			if (dir.isDirectory() && generated.exists()) {
 				System.out.println(generated);
 			}
 		}
-		
 	}
-
 }
