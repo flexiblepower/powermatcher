80c61480643c9ba66c16246feca354e9d711a8f2
Thijl Boonen <thijl.boonen@cgi.com>
2014-11-13 11:22:51 +0100

    Added basic CSVLogger by copying the SimpleObserver code

---
+++ net.powermatcher.core/src/net/powermatcher/core/monitoring/CSVLogger.java
@@ -0,0 +1,94 @@
+package net.powermatcher.core.monitoring;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import net.powermatcher.api.monitoring.ObservableAgent;
+import net.powermatcher.api.monitoring.AgentEvent;
+import net.powermatcher.core.monitoring.BaseObserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Modified;
+import aQute.bnd.annotation.component.Reference;
+import aQute.bnd.annotation.metatype.Configurable;
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * Example Observer which simply writes log entries of received events.
+ */
+@Component(immediate = true, designateFactory = CSVLogger.Config.class)
+public class CSVLogger extends BaseObserver {
+
+    private static final Logger logger = LoggerFactory.getLogger(CSVLogger.class);
+
+    private List<String> filter;
+
+    /**
+     * OSGI configuration of the {@link CSVLogger}
+     */
+    public static interface Config {
+        @Meta.AD(required = false)
+        List<String> filter();
+    }
+
+    /**
+     * Activate the component.
+     * 
+     * @param properties
+     *            updated configuration properties
+     */
+    @Activate
+    public synchronized void activate(Map<String, Object> properties) {
+        processConfig(properties);
+    }
+
+    /**
+     * Handle configuration modifications.
+     * 
+     * @param properties
+     *            updated configuration properties
+     */
+    @Modified
+    public synchronized void modified(Map<String, Object> properties) {
+        processConfig(properties);
+    }
+
+    @Override
+    @Reference(dynamic = true, multiple = true, optional = true)
+    public void addObservable(ObservableAgent observable, Map<String, Object> properties) {
+        super.addObservable(observable, properties);
+    }
+
+    //TODO this method only calls the superclass. is it missing an annotation?
+    @Override
+    public void removeObservable(ObservableAgent observable, Map<String, Object> properties) {
+        super.removeObservable(observable, properties);
+    }
+
+    @Override
+    protected List<String> filter() {
+        return this.filter;
+    }
+
+    @Override
+    public void update(AgentEvent event) {
+        logger.info("Received event: {}", event);
+    }
+
+    private void processConfig(Map<String, Object> properties) {
+        Config config = Configurable.createConfigurable(Config.class, properties);
+        this.filter = config.filter();
+
+        // ConfigAdmin will sometimes generate a filter with 1 empty element. Ignore it.
+        if (filter != null && !filter.isEmpty() && filter.get(0).isEmpty()) {
+            this.filter = new ArrayList<String>();
+        }
+
+        updateObservables();
+    }
+}
