2921654ef0f0b5f4b83572131f415fec0d1a49c4
THoeken <tom.hoeken@cgi.com>
2014-11-18 15:22:54 +0100

    Accidentally added IntegrationSuite to itself, this should be AuctioneerSiute

--- net.powermatcher.integration/src/net/powermatcher/integration/testsuites/IntegrationTestSuite.java
+++ net.powermatcher.integration/src/net/powermatcher/integration/testsuites/IntegrationTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ IntegrationTestSuite.class, ConcentratorTestSuite.class })
+@Suite.SuiteClasses({ AuctioneerTestSuite.class, ConcentratorTestSuite.class })
 public class IntegrationTestSuite {
 
 }
