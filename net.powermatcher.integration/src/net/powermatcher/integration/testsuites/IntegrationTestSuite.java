e1a9ba5efcf5fa2474eabf90e5dcd1bf8314b97b
THoeken <tom.hoeken@cgi.com>
2014-11-18 11:34:02 +0100

    Added and edited TestSuites for integration tests

--- net.powermatcher.integration/src/net/powermatcher/integration/testsuites/IntegrationTestSuite.java
+++ net.powermatcher.integration/src/net/powermatcher/integration/testsuites/IntegrationTestSuite.java
@@ -1,16 +1,10 @@
 package net.powermatcher.integration.testsuites;
 
-import net.powermatcher.integration.auctioneer.AuctioneerResilienceAFPriceTests;
-import net.powermatcher.integration.auctioneer.AuctioneerResilienceTestAF;
-import net.powermatcher.integration.auctioneer.AuctioneerResilienceTestIAQ1;
-import net.powermatcher.integration.auctioneer.AuctioneerResilienceTestIAQ2;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AuctioneerResilienceAFPriceTests.class, AuctioneerResilienceTestAF.class,
-        AuctioneerResilienceTestIAQ1.class, AuctioneerResilienceTestIAQ2.class })
+@Suite.SuiteClasses({ IntegrationTestSuite.class, ConcentratorTestSuite.class })
 public class IntegrationTestSuite {
 
 }
