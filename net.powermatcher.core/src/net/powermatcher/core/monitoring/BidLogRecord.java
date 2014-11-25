057672604a49fe36a8d9dd32d8150bfe808f9df0
Thijl Boonen <thijl.boonen@cgi.com>
2014-11-25 16:27:45 +0100

    fixes #110

--- net.powermatcher.core/src/net/powermatcher/core/monitoring/BidLogRecord.java
+++ net.powermatcher.core/src/net/powermatcher/core/monitoring/BidLogRecord.java
@@ -23,31 +23,30 @@ public class BidLogRecord extends LogRecord {
         MarketBasis marketBasis = bid.getMarketBasis();
 
         StringBuilder demandBuilder = new StringBuilder();
-        StringBuilder priceBuiler = new StringBuilder();
-        
+        StringBuilder pricePointBuiler = new StringBuilder();
+
         for (Double d : this.bid.getDemand()) {
             if (demandBuilder.length() > 0) {
                 demandBuilder.append("#");
             }
             demandBuilder.append(d);
         }
-        
-        
-        this.bid.getPricePoints()
-        
-        for (PricePoint p : this.bid.getPricePoints()) {
-            
-           
-            if (demandBuilder.length() > 0) {
-                demandBuilder.append("#");
+
+        if (bid.getPricePoints() != null) {
+
+            for (PricePoint p : this.bid.getPricePoints()) {
+                if (pricePointBuiler.length() > 0) {
+                    pricePointBuiler.append("|");
+                }
+
+                int priceStep = marketBasis.toPriceStep(p.getNormalizedPrice());
+                pricePointBuiler.append(MarketBasis.PRICE_FORMAT.format(marketBasis.toPrice(priceStep)));
+                pricePointBuiler.append("|").append(MarketBasis.DEMAND_FORMAT.format(p.getDemand()));
             }
-            demandBuilder.append(p);
         }
 
         return new String[] { getDateFormat().format(getLogTime()), getClusterId(), getAgentId(),
-                getQualifier().getDescription(),
-                marketBasis.getCommodity(),
-                marketBasis.getCurrency(),
+                getQualifier().getDescription(), marketBasis.getCommodity(), marketBasis.getCurrency(),
                 MarketBasis.PRICE_FORMAT.format(marketBasis.getMinimumPrice()),
                 MarketBasis.PRICE_FORMAT.format(marketBasis.getMaximumPrice()),
                 MarketBasis.DEMAND_FORMAT.format(this.bid.getMinimumDemand()),
@@ -56,7 +55,6 @@ public class BidLogRecord extends LogRecord {
                 MarketBasis.DEMAND_FORMAT.format(0),
                 // TODO where/what is the "effective price"?
                 MarketBasis.PRICE_FORMAT.format(0), getDateFormat().format(getEventTimestamp()),
-                String.valueOf(this.bid.getBidNumber()), demandBuilder.toString(),
-                this.bid.toString() };
+                String.valueOf(this.bid.getBidNumber()), demandBuilder.toString(), pricePointBuiler.toString() };
     }
 }
