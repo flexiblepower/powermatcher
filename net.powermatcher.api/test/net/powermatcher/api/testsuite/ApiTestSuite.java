341396e7a752c0e099c82235b5ff2c8f8f0ae870
Tom Hoeken <tom.hoeken@cgi.com>
2014-12-15 16:27:34 +0100

    Updated ApiTestSuite with PointBidTest

--- net.powermatcher.api/test/net/powermatcher/api/testsuite/ApiTestSuite.java
+++ net.powermatcher.api/test/net/powermatcher/api/testsuite/ApiTestSuite.java
@@ -2,6 +2,7 @@ package net.powermatcher.api.testsuite;
 
 import net.powermatcher.api.data.test.ArrayBidTest;
 import net.powermatcher.api.data.test.MarketBasisTest;
+import net.powermatcher.api.data.test.PointBidTest;
 import net.powermatcher.api.data.test.PriceTest;
 import net.powermatcher.api.monitoring.test.EventTest;
 
@@ -9,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ArrayBidTest.class, PriceTest.class, EventTest.class, MarketBasisTest.class })
+@Suite.SuiteClasses({ ArrayBidTest.class, PointBidTest.class, PriceTest.class, EventTest.class, MarketBasisTest.class })
 public class ApiTestSuite {
 
 }
