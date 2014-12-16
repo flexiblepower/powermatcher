2198acc3e08ba9992a2f3bcfb1f8ac25d0f373f9
Thijl Boonen <thijl.boonen@cgi.com>
2014-12-16 14:25:11 +0100

    Added WhiteListEvent/Record and PeakShavingEvent/Record and refactored AgentEvent/Record and existing subclasses

--- net.powermatcher.core/src/net/powermatcher/core/monitoring/PriceUpdateLogRecord.java
+++ net.powermatcher.core/src/net/powermatcher/core/monitoring/PriceUpdateLogRecord.java
@@ -4,19 +4,31 @@ import java.text.DateFormat;
 import java.util.Date;
 
 import net.powermatcher.api.data.PriceUpdate;
+import net.powermatcher.api.monitoring.Qualifier;
+import net.powermatcher.api.monitoring.events.AgentEvent;
 import net.powermatcher.api.monitoring.events.PriceUpdateEvent;
 
 public class PriceUpdateLogRecord extends LogRecord {
 
     private PriceUpdate priceUpdate;
+    /**
+     * The qualifier of the agent that published the {@link AgentEvent}.
+     */
+    private Qualifier qualifier;
+
 
     public PriceUpdateLogRecord(PriceUpdateEvent event, Date logTime, DateFormat dateFormat) {
 
-        super(event.getClusterId(), event.getAgentId(), event.getQualifier(), logTime, event.getTimestamp(), dateFormat);
+        super(event.getClusterId(), event.getAgentId(), logTime, event.getTimestamp(), dateFormat);
         this.priceUpdate = event.getPriceUpdate();
+        this.qualifier = event.getQualifier();
     }
 
     protected PriceUpdate getPriceUpdate() {
         return priceUpdate;
+    }
+    
+    public Qualifier getQualifier() {
+        return qualifier;
     }
 }
