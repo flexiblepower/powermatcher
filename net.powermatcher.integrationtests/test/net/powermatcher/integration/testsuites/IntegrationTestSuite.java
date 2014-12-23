6e085ebcc5cd397ee724c7858ec097cfc60302c3
Tom Hoeken <tom.hoeken@cgi.com>
2014-12-23 15:54:48 +0100

    Added test to assert that powermatcher has working methods to prevent oscillation.

--- net.powermatcher.integrationtests/test/net/powermatcher/integration/testsuites/IntegrationTestSuite.java
+++ net.powermatcher.integrationtests/test/net/powermatcher/integration/testsuites/IntegrationTestSuite.java
@@ -1,10 +1,13 @@
 package net.powermatcher.integration.testsuites;
 
+import net.powermatcher.integration.oscillation.OscillationPreventionTest;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AuctioneerTestSuite.class, ConcentratorTestSuite.class, CommunicationTestSuite.class })
+@Suite.SuiteClasses({ AuctioneerTestSuite.class, ConcentratorTestSuite.class, CommunicationTestSuite.class,
+        OscillationPreventionTest.class })
 public class IntegrationTestSuite {
 
 }
