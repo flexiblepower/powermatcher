5eff59da9a0821150151b34f60f9926bce113639
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2015-01-22 15:32:41 +0100

    Cleaned up LoggingScheduler

--- net.powermatcher.runtime/src/net/powermatcher/core/time/LoggingScheduler.java
+++ net.powermatcher.runtime/src/net/powermatcher/core/time/LoggingScheduler.java
@@ -15,7 +15,6 @@ import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Deactivate;
 
 /**
