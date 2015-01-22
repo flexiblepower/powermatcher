1bde9d19be6a57b367a7591b53218bbf27f34bd9
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2015-01-22 15:16:12 +0100

    fixed errors due to change

--- net.powermatcher.extensions/src/net/powermatcher/extensions/connectivity/websockets/MatcherEndpointProxyWebsocket.java
+++ net.powermatcher.extensions/src/net/powermatcher/extensions/connectivity/websockets/MatcherEndpointProxyWebsocket.java
@@ -9,6 +9,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import net.powermatcher.api.MatcherEndpoint;
+import net.powermatcher.api.TimeService;
 import net.powermatcher.api.data.Bid;
 import net.powermatcher.api.data.PriceUpdate;
 import net.powermatcher.api.monitoring.ObservableAgent;
@@ -40,225 +41,232 @@ import aQute.bnd.annotation.metatype.Meta;
 import com.google.gson.JsonSyntaxException;
 
 /**
- * WebSocket implementation of an {@link MatcherEndpointProxy}. Enabled two agents to communicate via WebSockets and
- * JSON over a TCP connection.
+ * WebSocket implementation of an {@link MatcherEndpointProxy}. Enabled two
+ * agents to communicate via WebSockets and JSON over a TCP connection.
  * 
  * @author FAN
  * @version 2.0
  */
 @WebSocket()
 @Component(designateFactory = MatcherEndpointProxyWebsocket.Config.class, immediate = true, provide = {
-        ObservableAgent.class, MatcherEndpoint.class, BaseMatcherEndpointProxy.class })
+		ObservableAgent.class, MatcherEndpoint.class,
+		BaseMatcherEndpointProxy.class })
 public class MatcherEndpointProxyWebsocket extends BaseMatcherEndpointProxy {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MatcherEndpointProxyWebsocket.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(MatcherEndpointProxyWebsocket.class);
 
-    @Meta.OCD
-    public static interface Config {
-        @Meta.AD(deflt = "", description = "remote agent endpoint proxy to connect to.")
-        String desiredConnectionId();
+	@Meta.OCD
+	public static interface Config {
+		@Meta.AD(deflt = "", description = "remote agent endpoint proxy to connect to.")
+		String desiredConnectionId();
 
-        @Meta.AD(deflt = "matcherendpointproxy", description = "local agent identification")
-        String agentId();
+		@Meta.AD(deflt = "matcherendpointproxy", description = "local agent identification")
+		String agentId();
 
-        @Meta.AD(
-                deflt = "ws://localhost:8080/powermatcher/websockets/agentendpoint",
-                description = "URL of powermatcher websocket endpoint.")
-        String powermatcherUrl();
+		@Meta.AD(deflt = "ws://localhost:8080/powermatcher/websockets/agentendpoint", description = "URL of powermatcher websocket endpoint.")
+		String powermatcherUrl();
 
-        @Meta.AD(deflt = "30", description = "reconnect timeout keeping the connection alive.")
-        int reconnectTimeout();
+		@Meta.AD(deflt = "30", description = "reconnect timeout keeping the connection alive.")
+		int reconnectTimeout();
 
-        @Meta.AD(deflt = "60", description = "connect timeout to wait for remote server to respond.")
-        int connectTimeout();
-    }
+		@Meta.AD(deflt = "60", description = "connect timeout to wait for remote server to respond.")
+		int connectTimeout();
+	}
 
-    private URI powermatcherUrl;
+	private URI powermatcherUrl;
 
-    private org.eclipse.jetty.websocket.api.Session remoteSession;
+	private org.eclipse.jetty.websocket.api.Session remoteSession;
 
-    private WebSocketClient client;
+	private WebSocketClient client;
 
-    private int connectTimeout;
+	private int connectTimeout;
 
-    /**
-     * OSGi calls this method to activate a managed service.
-     * 
-     * @param properties
-     *            the configuration properties
-     */
-    @Activate
-    public synchronized void activate(Map<String, Object> properties) {
-        // Read configuration properties
-        Config config = Configurable.createConfigurable(Config.class, properties);
-        this.setAgentId(config.agentId());
+	/**
+	 * OSGi calls this method to activate a managed service.
+	 * 
+	 * @param properties
+	 *            the configuration properties
+	 */
+	@Activate
+	public synchronized void activate(Map<String, Object> properties) {
+		// Read configuration properties
+		Config config = Configurable.createConfigurable(Config.class,
+				properties);
+		this.setAgentId(config.agentId());
 
-        try {
-            this.powermatcherUrl = new URI(config.powermatcherUrl() + "?agentId=" + this.getAgentId()
-                    + "&desiredConnectionId=" + config.desiredConnectionId());
-        } catch (URISyntaxException e) {
-            LOGGER.error("Malformed URL for powermatcher websocket endpoint. Reason {}", e);
-            return;
-        }
+		try {
+			this.powermatcherUrl = new URI(config.powermatcherUrl()
+					+ "?agentId=" + this.getAgentId() + "&desiredConnectionId="
+					+ config.desiredConnectionId());
+		} catch (URISyntaxException e) {
+			LOGGER.error(
+					"Malformed URL for powermatcher websocket endpoint. Reason {}",
+					e);
+			return;
+		}
 
-        this.baseActivate(config.reconnectTimeout());
-        this.connectTimeout = config.connectTimeout();
-    }
+		this.baseActivate(config.reconnectTimeout());
+		this.connectTimeout = config.connectTimeout();
+	}
 
-    /**
-     * OSGi calls this method to deactivate a managed service.
-     */
-    @Deactivate
-    public synchronized void deactivate() {
-        this.baseDeactivate();
-    }
+	/**
+	 * OSGi calls this method to deactivate a managed service.
+	 */
+	@Deactivate
+	public synchronized void deactivate() {
+		this.baseDeactivate();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Reference
-    @Override
-    public void setExecutorService(ScheduledExecutorService scheduler) {
-        super.setExecutorService(scheduler);
-    }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * This specific implementation opens a websocket.
+	 */
+	@Override
+	public synchronized boolean connectRemote() {
+		if (!this.isLocalConnected()) {
+			// Don't connect when no agentRole is connected
+			return true;
+		}
 
-    /**
-     * {@inheritDoc}
-     * 
-     * This specific implementation opens a websocket.
-     */
-    @Override
-    public synchronized boolean connectRemote() {
-        if (!this.isLocalConnected()) {
-            // Don't connect when no agentRole is connected
-            return true;
-        }
+		if (this.isRemoteConnected()) {
+			// Already connected, skip connection
+			return true;
+		}
 
-        if (this.isRemoteConnected()) {
-            // Already connected, skip connection
-            return true;
-        }
+		return connectWebsocket();
+	}
 
-        return connectWebsocket();
-    }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * This specific implementation closes the open websocket.
+	 */
+	@Override
+	public synchronized boolean disconnectRemote() {
+		// Terminate remote session (if any)
+		if (this.isRemoteConnected()) {
+			this.remoteSession.close(new CloseStatus(0, "Normal disconnect"));
+		}
 
-    /**
-     * {@inheritDoc}
-     * 
-     * This specific implementation closes the open websocket.
-     */
-    @Override
-    public synchronized boolean disconnectRemote() {
-        // Terminate remote session (if any)
-        if (this.isRemoteConnected()) {
-            this.remoteSession.close(new CloseStatus(0, "Normal disconnect"));
-        }
+		this.remoteSession = null;
 
-        this.remoteSession = null;
+		// Stop the client
+		if (this.client != null && !this.client.isStopped()) {
+			try {
+				this.client.stop();
+			} catch (Exception e) {
+				LOGGER.warn("Unable to disconnect, reason: [{}]", e);
+				return false;
+			}
+		}
 
-        // Stop the client
-        if (this.client != null && !this.client.isStopped()) {
-            try {
-                this.client.stop();
-            } catch (Exception e) {
-                LOGGER.warn("Unable to disconnect, reason: [{}]", e);
-                return false;
-            }
-        }
+		return true;
+	}
 
-        return true;
-    }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * This specific implementation checks to see if the websocket is open.
+	 */
+	@Override
+	public boolean isRemoteConnected() {
+		return this.remoteSession != null && this.remoteSession.isOpen();
+	}
 
-    /**
-     * {@inheritDoc}
-     * 
-     * This specific implementation checks to see if the websocket is open.
-     */
-    @Override
-    public boolean isRemoteConnected() {
-        return this.remoteSession != null && this.remoteSession.isOpen();
-    }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * This specific implementation serializes the {@link Bid} to json and sends
+	 * it through the websocket.
+	 */
+	@Override
+	public synchronized void updateBidRemote(Bid newBid) {
+		// Relay bid to remote agent
+		try {
+			PmJsonSerializer serializer = new PmJsonSerializer();
+			String message = serializer.serializeBid(newBid);
+			this.remoteSession.getRemote().sendString(message);
+		} catch (IOException e) {
+			LOGGER.error("Unable to send new bid to remote agent. Reason {}", e);
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     * 
-     * This specific implementation serializes the {@link Bid} to json and sends it through the websocket.
-     */
-    @Override
-    public synchronized void updateBidRemote(Bid newBid) {
-        // Relay bid to remote agent
-        try {
-            PmJsonSerializer serializer = new PmJsonSerializer();
-            String message = serializer.serializeBid(newBid);
-            this.remoteSession.getRemote().sendString(message);
-        } catch (IOException e) {
-            LOGGER.error("Unable to send new bid to remote agent. Reason {}", e);
-        }
-    }
+	@OnWebSocketConnect
+	public void onConnect(org.eclipse.jetty.websocket.api.Session session) {
+		LOGGER.info("Connected: {}", session);
+	}
 
-    @OnWebSocketConnect
-    public void onConnect(org.eclipse.jetty.websocket.api.Session session) {
-        LOGGER.info("Connected: {}", session);
-    }
+	@OnWebSocketClose
+	public void onDisconnect(int statusCode, String reason) {
+		LOGGER.info("Connection closed: {} - {}", statusCode, reason);
 
-    @OnWebSocketClose
-    public void onDisconnect(int statusCode, String reason) {
-        LOGGER.info("Connection closed: {} - {}", statusCode, reason);
+		this.remoteSession = null;
+	}
 
-        this.remoteSession = null;
-    }
+	@OnWebSocketMessage
+	public void onMessage(String message) {
+		LOGGER.debug("Received message from remote agent {}", message);
 
-    @OnWebSocketMessage
-    public void onMessage(String message) {
-        LOGGER.debug("Received message from remote agent {}", message);
+		try {
+			// Decode the JSON data
+			PmJsonSerializer serializer = new PmJsonSerializer();
+			PmMessage pmMessage = serializer.deserialize(message);
 
-        try {
-            // Decode the JSON data
-            PmJsonSerializer serializer = new PmJsonSerializer();
-            PmMessage pmMessage = serializer.deserialize(message);
+			// Handle specific message
+			if (pmMessage.getPayloadType() == PayloadType.PRICE_UPDATE) {
+				// Relay price update to local agent
+				PriceUpdate newPriceUpdate = ModelMapper
+						.mapPriceUpdate((PriceUpdateModel) pmMessage
+								.getPayload());
+				this.updateLocalPrice(newPriceUpdate);
+			}
 
-            // Handle specific message
-            if (pmMessage.getPayloadType() == PayloadType.PRICE_UPDATE) {
-                // Relay price update to local agent
-                PriceUpdate newPriceUpdate = ModelMapper.mapPriceUpdate((PriceUpdateModel) pmMessage.getPayload());
-                this.updateLocalPrice(newPriceUpdate);
-            }
+			if (pmMessage.getPayloadType() == PayloadType.CLUSTERINFO) {
+				// Sync marketbasis and clusterid with local session, for new
+				// connections
+				ClusterInfoModel clusterInfo = (ClusterInfoModel) pmMessage
+						.getPayload();
+				this.updateRemoteClusterId(clusterInfo.getClusterId());
+				this.updateRemoteMarketBasis(ModelMapper
+						.convertMarketBasis(clusterInfo.getMarketBasis()));
+			}
+		} catch (JsonSyntaxException e) {
+			LOGGER.warn("Unable to understand message from remote agent: {}",
+					message);
+		}
+	}
 
-            if (pmMessage.getPayloadType() == PayloadType.CLUSTERINFO) {
-                // Sync marketbasis and clusterid with local session, for new
-                // connections
-                ClusterInfoModel clusterInfo = (ClusterInfoModel) pmMessage.getPayload();
-                this.updateRemoteClusterId(clusterInfo.getClusterId());
-                this.updateRemoteMarketBasis(ModelMapper.convertMarketBasis(clusterInfo.getMarketBasis()));
-            }
-        } catch (JsonSyntaxException e) {
-            LOGGER.warn("Unable to understand message from remote agent: {}", message);
-        }
-    }
+	private boolean connectWebsocket() {
+		// Try to setup a new websocket connection.
+		client = new WebSocketClient();
+		ClientUpgradeRequest request = new ClientUpgradeRequest();
 
-    private boolean connectWebsocket() {
-        // Try to setup a new websocket connection.
-        client = new WebSocketClient();
-        ClientUpgradeRequest request = new ClientUpgradeRequest();
+		try {
+			client.start();
+			Future<org.eclipse.jetty.websocket.api.Session> connectFuture = client
+					.connect(this, this.powermatcherUrl, request);
+			LOGGER.info("Connecting to : {}", request);
 
-        try {
-            client.start();
-            Future<org.eclipse.jetty.websocket.api.Session> connectFuture = client.connect(this, this.powermatcherUrl,
-                    request);
-            LOGGER.info("Connecting to : {}", request);
+			// Wait configurable time for remote to respond
+			org.eclipse.jetty.websocket.api.Session newRemoteSession = connectFuture
+					.get(this.connectTimeout, TimeUnit.SECONDS);
 
-            // Wait configurable time for remote to respond
-            org.eclipse.jetty.websocket.api.Session newRemoteSession = connectFuture.get(this.connectTimeout,
-                    TimeUnit.SECONDS);
+			this.remoteSession = newRemoteSession;
+		} catch (Exception e) {
+			LOGGER.error("Unable to connect to remote agent. Reason {}", e);
 
-            this.remoteSession = newRemoteSession;
-        } catch (Exception e) {
-            LOGGER.error("Unable to connect to remote agent. Reason {}", e);
+			this.remoteSession = null;
+			return false;
+		}
 
-            this.remoteSession = null;
-            return false;
-        }
+		return true;
+	}
 
-        return true;
-    }
+	@Override
+	public void setTimeService(TimeService timeService) {
+		// TODO Auto-generated method stub
+
+	}
 }
