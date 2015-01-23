88f1b3e671aca6041a722cde629b2b94413d3432
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2015-01-23 16:25:11 +0100

    Fixed ConcentratorTest

--- net.powermatcher.core/test/net/powermatcher/core/testsuite/CoreTestSuite.java
+++ net.powermatcher.core/test/net/powermatcher/core/testsuite/CoreTestSuite.java
@@ -6,7 +6,6 @@ import net.powermatcher.core.concentrator.test.ConcentratorTest;
 import net.powermatcher.core.concentrator.test.PeakShavingConcentratorTest;
 import net.powermatcher.core.monitoring.test.LogRecordTest;
 import net.powermatcher.core.monitoring.test.ObservableAgentTest;
-import net.powermatcher.core.sessions.test.SessionManagerTest;
 import net.powermatcher.core.test.BaseDeviceAgentTest;
 import net.powermatcher.core.test.BidCacheElementTest;
 import net.powermatcher.core.test.BidCacheTest;
@@ -21,9 +20,11 @@ import org.junit.runners.Suite;
  * @version 2.0
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AuctioneerTest.class, ObjectiveAuctioneerTest.class, ConcentratorTest.class,
-        PeakShavingConcentratorTest.class, LogRecordTest.class, ObservableAgentTest.class, SessionManagerTest.class,
-        BaseDeviceAgentTest.class, BidCacheElementTest.class, BidCacheTest.class })
+@Suite.SuiteClasses({ AuctioneerTest.class, ObjectiveAuctioneerTest.class,
+		ConcentratorTest.class, PeakShavingConcentratorTest.class,
+		LogRecordTest.class, ObservableAgentTest.class,
+		BaseDeviceAgentTest.class, BidCacheElementTest.class,
+		BidCacheTest.class })
 public class CoreTestSuite {
 
 }
