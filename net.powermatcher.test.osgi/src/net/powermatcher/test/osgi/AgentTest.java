83b980a6f2f1c54102d106cf553e60aace61a214
bjornamkreutz <amkreutz.harst@ziggo.nl>
2015-03-11 15:16:17 +0100

    added configAdmin

--- net.powermatcher.test.osgi/src/net/powermatcher/test/osgi/AgentTest.java
+++ net.powermatcher.test.osgi/src/net/powermatcher/test/osgi/AgentTest.java
@@ -1,19 +1,23 @@
 package net.powermatcher.test.osgi;
 
 import junit.framework.TestCase;
+import net.powermatcher.core.auctioneer.Auctioneer;
 
+import org.apache.felix.scr.ScrService;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.ConfigurationAdmin;
+import org.apache.felix.scr.Component;
 
 public class AgentTest extends TestCase {
 
     private final BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
 
-    @SuppressWarnings("unused")
-	private ConfigurationAdmin configAdmin;
+    private ConfigurationAdmin configAdmin;
     private ServiceReference<?> configAdminReference;
+    private ScrService scrService;
+    private ServiceReference scrServiceReference; 
     
 	public void testExample() throws Exception {
 		super.setUp();
@@ -24,6 +28,16 @@ public class AgentTest extends TestCase {
     	}
     	assert(configAdmin != null);
     	assertNotNull(configAdmin);
+    	
+    	scrServiceReference = context.getServiceReference( ScrService.class.getName() );
+    	if (scrServiceReference != null) {
+    		scrService = (ScrService) context.getService(scrServiceReference);
+    	}
+
+    	assertNotNull(scrService);
+    	
+    	// Check if the ConfigManager is present
+    	Component[] components = scrService.getComponents("net.powermatcher.core.config.management.agent.ConfigManager");
     }
 	
 }
