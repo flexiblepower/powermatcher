cec006c324ff6a447a684e1ac238478bf5ebb75a
Thijl Boonen <thijl.boonen@cgi.com>
2015-03-13 16:12:59 +0100

    Added the StoringObserver

--- net.powermatcher.test.osgi/src/net/powermatcher/test/osgi/ClusterTest.java
+++ net.powermatcher.test.osgi/src/net/powermatcher/test/osgi/ClusterTest.java
@@ -20,6 +20,7 @@ import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.util.tracker.ServiceTracker;
 
 import net.powermatcher.examples.PVPanelAgent;
+import net.powermatcher.examples.StoringObserver;
 
 public class ClusterTest extends TestCase {
 
@@ -70,10 +71,31 @@ public class ClusterTest extends TestCase {
     	// check PvPanel alive
     	assertEquals(true, pvPanelActive());
     	
-    	disconnectPvPanel(configAdmin);
+    	//Create StoringObserver
+    	String storingObserverFactoryPid = "net.powermatcher.examples.StoringObserver";
+    	Configuration storingObserverConfig = configAdmin.createFactoryConfiguration(storingObserverFactoryPid, null);
+
+    	setStoringObserverProperties(storingObserverConfig);
+    	
+    	// Wait for StoringObserver to become active
+    	StoringObserver observer = getServiceByPid(storingObserverConfig.getPid(), StoringObserver.class);
+    	
+    	//3.	Testen of alle agents biedingen sturen 
+    	
+    	//Checking to see if all agents send bids
+    	Thread.sleep(30000);
+    	
+    	checkBidsFullCluster(observer);
+    	
+    	//Disconnecting the PVPanel
+       	disconnectPvPanel(configAdmin);
     	
     	// check PvPanel not active anymore
     	assertFalse(testPvPanelNotActive());
+    	
+    	//CHecking to see is the PVPanel stopped sending bid
+    	observer.clearEvents();
+    	Thread.sleep(30000);
     }
 
 	private boolean testPvPanelNotActive() throws Exception {
@@ -208,6 +230,26 @@ public class ClusterTest extends TestCase {
     	properties.put("priceUpdateRate", 30l);
     	properties.put("minTimeBetweenPriceUpdates", 1000);
     	auctioneerConfig.update(properties);
+    }
+    
+    private void setStoringObserverProperties(Configuration storingObserverConfig) throws Exception {
+    	Dictionary<String, Object> properties = new Hashtable<String, Object>();
+    	properties.put("observableAgent_filter", "");
+    	storingObserverConfig.update(properties);
+    }
+    
+    private void checkBidsFullCluster(StoringObserver observer)
+    {
+    	assert(!observer.getEvents().isEmpty());
+    	assert(observer.getEvents().containsKey("concentrator"));
+    	assert(observer.getEvents().containsKey("pvpanel"));
+    }
+    
+    private void checkBidsClusterNoPVPabel(StoringObserver observer)
+    {
+    	assert(!observer.getEvents().isEmpty());
+    	assert(observer.getEvents().containsKey("concentrator"));
+    	assertFalse(observer.getEvents().containsKey("pvpanel"));
     }
     
 }
