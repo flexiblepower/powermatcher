4f7d62a864b3254f3e23f64fd19314af999240cc
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2016-03-04 13:10:21 +0100

    Improved the PowerMatcherContext, threads are now named

--- net.powermatcher.runtime/src/net/powermatcher/runtime/PowerMatcherContext.java
+++ net.powermatcher.runtime/src/net/powermatcher/runtime/PowerMatcherContext.java
@@ -4,11 +4,14 @@ import java.util.Date;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.RunnableScheduledFuture;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.measure.Measurable;
 import javax.measure.quantity.Duration;
@@ -19,6 +22,11 @@ import org.flexiblepower.context.FlexiblePowerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The runtime implementation of {@link FlexiblePowerContext}. This implementation uses a
+ * {@link ScheduledThreadPoolExecutor} in order to execute tasks. You would typically use a different implementation in
+ * a simulated environment.
+ */
 public class PowerMatcherContext
     extends ScheduledThreadPoolExecutor
     implements FlexiblePowerContext {
@@ -27,6 +35,11 @@ public class PowerMatcherContext
 
     private static final Logger logger = LoggerFactory.getLogger(PowerMatcherContext.class);
 
+    /**
+     * This class wraps a task and catches and logs exceptions that might occur. Normally, a scheduled tasks gets
+     * cancelled when it throws an exception. Since PowerMatcher needs to be robust, we need to continue even if an
+     * exception gets thrown.
+     */
     static class WrappedTask<T>
         implements RunnableScheduledFuture<T> {
         private final RunnableScheduledFuture<T> task;
@@ -80,14 +93,25 @@ public class PowerMatcherContext
             try {
                 task.run();
             } catch (Exception ex) {
+                // The Exception is not thrown again to avoid this task being cancelled
                 logger.error("An scheduled execution has thrown an exception: " + ex.getMessage(), ex);
-                // TODO: should we rethrow this exception or not? Now it will retry to execute the task
             }
         }
     }
 
     public PowerMatcherContext() {
-        super(Runtime.getRuntime().availableProcessors() * 2);
+        // We provide a ThreadFactor so we can name the Threads, which makes debugging easier
+        super(Runtime.getRuntime().availableProcessors() + 1, new ThreadFactory() {
+
+            private final AtomicInteger cnt = new AtomicInteger(0);
+
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread newThread = Executors.defaultThreadFactory().newThread(r);
+                newThread.setName("PowerMatcherRuntime-" + cnt.getAndIncrement());
+                return newThread;
+            }
+        });
         setKeepAliveTime(5, TimeUnit.MINUTES);
     }
 
